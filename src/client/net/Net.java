d98f8f91bdcec4b9dadb8ccad836bfdc4669d1b6
Oscar Falkman <ofalkman@gmail.com>
2017-11-08 16:01:34 +0100

    Small client net fixes.

--- src/client/net/Net.java
+++ src/client/net/Net.java
@@ -8,42 +8,44 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Net {
-private Socket socket;
-private PrintWriter sentMessage;
-private BufferedReader receivedMessage;
-private int port = 5555;
-private static final int TIME_OUT_ONE_MINUTE = 60000;
-private String disconnectMessage = "exit-game"
+    private Socket socket;
+    private PrintWriter sentMessage;
+    private BufferedReader receivedMessage;
+    private int port = 5555;
+    private static final int TIME_OUT_ONE_MINUTE = 60000;
+    private String disconnectMessage = "exit-game";
 
 
- public void newConnection(String host) throws IOException {
-     socket = new Socket();
-     socket.connect(new InetSocketAddress(host, port), TIME_OUT_ONE_MINUTE);
-     boolean autoflush = true;
-     sentMessage = new PrintWriter(socket.getOutputStream(), autoflush);
-     receivedMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-     new Thread(new Listener)
- }
+    public void newConnection(String host) throws IOException {
+        socket = new Socket();
+        socket.connect(new InetSocketAddress(host, port), TIME_OUT_ONE_MINUTE);
+        boolean autoflush = true;
+        sentMessage = new PrintWriter(socket.getOutputStream(), autoflush);
+        receivedMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        Listener listener = new Listener();
+        new Thread(listener).run();
+    }
 
- public void disconnect() throws IOException {
-     sendMessage(disconnectMessage);
-     socket.close();
-     socket = null;
+    public void disconnect() throws IOException {
+        sendMessage(disconnectMessage);
+        socket.close();
+        socket = null;
 
- }
+    }
 
- public void sendMessage(String message) {
-     sentMessage.println(message);
- }
-    private class Listener()implements Runnable {
+    public void sendMessage(String message) {
+        sentMessage.println(message);
+    }
+
+    private class Listener implements Runnable {
 
         private Listener() {
 
         }
 
+        @Override
+        public void run() {
 
+        }
     }
-
-
-
 }
