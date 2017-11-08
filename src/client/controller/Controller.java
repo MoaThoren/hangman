714901ca8f37b280f4b2713ea15a9c10b1bb5fff
Joakim Markén <jmarken@kth.se>
2017-11-08 15:04:34 +0100

    server och saker

--- src/client/controller/Controller.java
+++ src/client/controller/Controller.java
@@ -1,4 +1,7 @@
 package client.controller;
 
 public class Controller {
+    public static void String(){
+
+    }
 }
--- src/client/viewmodel/ViewModel.java
+++ src/client/viewmodel/ViewModel.java
@@ -1,32 +1,5 @@
 package client.viewmodel;
 
-import client.TempServer;
-
-import java.util.Scanner;
-
 public class ViewModel {
-    public static void input(){
-        boolean exit = false;
-        Scanner sc = new Scanner(System.in);
 
-        //TEMP
-        TempServer ts = new TempServer();
-
-        while(!exit){
-            String in = sc.next().toLowerCase();
-            switch (in){
-                case "exit-game":{
-                    System.out.println("Good bye!");
-                    exit = true;
-                }
-                default:
-                    if(!exit){
-                        //PASS ON TO SERVER AND GET RESPONSE
-                        System.out.println(ts.guessWord(in));
-
-                    }
-            }
-        }
-
-    }
 }
