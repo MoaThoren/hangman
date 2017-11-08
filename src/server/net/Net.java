eaafc24ea8287be8ed3b6f18edba325dcde8883d
Oscar Falkman <ofalkman@gmail.com>
2017-11-08 15:15:54 +0100

    Merge branches 'Dev' and 'master' of https://github.com/Osyx/hangman into Dev

--- src/server/net/Net.java
+++ src/server/net/Net.java
@@ -7,54 +7,63 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Net {
-    private int PORT_NUMBER = 8888;
+    private int PORT_NUMBER = 5555;
     private int LINGER_TIME = 5000;
-    private volatile boolean connected;
     private File rootDir = new File("/");
     private ServerSocket serverSocket;
-    private Socket socket;
     private PrintWriter output;
     private BufferedReader input;
-    private Controller controller;
+    private Controller controller = new Controller();
 
-    public Net() {
-        controller = new Controller();
-    }
-
-    private void serve() {
+    public void main(String[] args) {
         try {
             serverSocket = new ServerSocket(PORT_NUMBER);
         } catch (IOException e) {
             System.exit(1);
         }
-        handleClient();
+        newClientSocket();
     }
 
-    private void handleClient() {
-        try {
-            socket = serverSocket.accept();
-            socket.setSoLinger(true, LINGER_TIME);
-            output = new PrintWriter(socket.getOutputStream(),true);
-            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            receive();
-        } catch (IOException e) {
-            System.exit(1);
-        } finally {
+    private void newClientSocket() {
+        new NetThread().run();
+    }
+
+    private class NetThread extends Thread {
+        private Socket clientSocket;
+
+        public void run() {
             try {
-                socket.close();
-            } catch(IOException ioEx) {
+                clientSocket = serverSocket.accept();
+                newClientSocket();
+                clientSocket.setSoLinger(true, LINGER_TIME);
+                output = new PrintWriter(clientSocket.getOutputStream(),true);
+                input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                receive();
+            } catch (IOException e) {
                 System.exit(1);
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch(IOException ioEx) {
+                    System.exit(1);
+                }
             }
         }
-    }
 
-    private void receive() {
-        while(connected) {
-            try {
-                controller.checkString(input.readLine());
-            } catch (IOException e) {
-                e.printStackTrace();
+        private void receive() {
+            while(true) {
+                try {
+                    String reply = controller.checkString(input.readLine());
+                    if(reply.equalsIgnoreCase("exit-game")) break;
+                    send(reply);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+        }
+
+        private void send(String reply) {
+            output.println(reply);
         }
     }
 }
